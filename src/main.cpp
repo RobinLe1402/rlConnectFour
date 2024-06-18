ccda3bc89e3af0e8a3d22b6c945d952ed3746c40
RobinLe1402 <web@robinle.de>
2024-06-18 18:29:57 +0200

    added Win32-only code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <cstdio>
 
+#ifdef rlWINDOWS
+#define WIN32_LEAN_AND_MEAN
+#define NOCOMM
+#define NOMINMAX
+#include <Windows.h>
+#endif
+
 int main(int argc, char* argv[])
 {
+#ifdef rlWINDOWS
+	{
+		// on Windows, ANSI escape sequences must be manually enabled.
+		HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		DWORD dwMode = 0;
+		GetConsoleMode(hOut, &dwMode);
+		dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+		SetConsoleMode(hOut, dwMode);
+	}
+#endif
+
+
 	std::printf(
 		"==================================================\n"
 		"= ROBIN LE'S  CONNECT FOUR  FOR THE COMMAND LINE =\n"
