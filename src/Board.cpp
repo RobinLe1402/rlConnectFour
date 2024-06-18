1f59eb5dbd715376c049b4903187c295dee9a876
RobinLe1402 <web@robinle.de>
2024-06-18 20:29:54 +0200

    added Board.cpp and Board.hpp

---
+++ src/Board.cpp
@@ -0,0 +1,168 @@
+#include "Board.hpp"
+
+
+
+namespace rlConnectFour
+{
+
+	unsigned Board::getIndex(unsigned iColumn, unsigned iRowFromBottom) const
+	{
+		assert(iColumn        < BoardWidth);
+		assert(iRowFromBottom < BoardHeight);
+
+		return iColumn * BoardHeight + iRowFromBottom;
+	}
+
+	void Board::dropToken(Token ePlayer, unsigned iColumn)
+	{
+		assert(iColumn < BoardWidth);
+		assert(m_iTokenCount[iColumn] < BoardHeight);
+		assert(ePlayer != Token::None);
+		assert(m_eWinner == Token::None);
+
+		const auto idx = iColumn * BoardHeight + m_iTokenCount[iColumn];
+		m_eTokens[idx] = ePlayer;
+		++m_iTokenCount[iColumn];
+
+		checkWinner(idx);
+	}
+
+	void Board::checkWinner(size_t iLastDropped)
+	{
+		assert(iLastDropped < BoardSize);
+		assert(m_eTokens[iLastDropped] != Token::None);
+
+		const Token    eDropped = m_eTokens[iLastDropped];
+		const unsigned iDropCol = unsigned(iLastDropped / BoardHeight);
+		const unsigned iDropRow = unsigned(iLastDropped % BoardHeight);
+
+
+		// 1. check downwards
+		if (iDropRow >= 4)
+		{
+			if (
+				m_eTokens[iLastDropped - 1] == eDropped &&
+				m_eTokens[iLastDropped - 2] == eDropped &&
+				m_eTokens[iLastDropped - 3] == eDropped
+			)
+			{
+				m_eWinner = eDropped;
+				return;
+			}
+		}
+
+
+
+		// 2. check horizontal
+		unsigned iTokenCount;
+		size_t   idx;
+		int      iCol;
+		int      iRow;
+		{
+			iTokenCount = 1;
+
+			// <--X
+			for (
+				idx = iLastDropped - BoardHeight, iCol = iDropCol - 1;
+				iCol >= 0 && m_eTokens[idx] == eDropped;
+				--iCol, idx -= BoardHeight
+			)
+			{
+				++iTokenCount;
+			}
+
+			// X-->
+			for (
+				idx = iLastDropped + BoardHeight, iCol = iDropCol + 1;
+				iCol < BoardWidth && m_eTokens[idx] == eDropped;
+				++iCol, idx += BoardHeight
+			)
+			{
+				++iTokenCount;
+			}
+
+
+			if (iTokenCount >= 4)
+			{
+				m_eWinner = eDropped;
+				return;
+			}
+		}
+
+
+		// 3. check diagnoal 1 (bottom left to top right)
+		{
+			iTokenCount = 1;
+
+			// down left
+			for (
+				idx = iLastDropped - BoardHeight - 1,
+					iCol = iDropCol - 1,
+					iRow = iDropRow - 1;
+				iCol >= 0 && iRow >= 0 && m_eTokens[idx] == eDropped;
+				--iCol, --iRow, idx -= BoardHeight + 1
+			)
+			{
+				++iTokenCount;
+			}
+
+			// up right
+			for (
+				idx = iLastDropped + BoardHeight + 1,
+					iCol = iDropCol + 1,
+					iRow = iDropRow + 1;
+				iCol < BoardWidth && iRow < BoardHeight && m_eTokens[idx] == eDropped;
+				++iCol, ++iRow, idx += BoardHeight + 1
+			)
+			{
+				++iTokenCount;
+			}
+
+
+			if (iTokenCount >= 4)
+			{
+				m_eWinner = eDropped;
+				return;
+			}
+		}
+
+
+		// 4. check diagnoal 2 (top left to bottom right)
+		{
+			iTokenCount = 1;
+
+			// up left
+			for (
+				idx = iLastDropped - BoardHeight + 1,
+					iCol = iDropCol - 1,
+					iRow = iDropRow + 1;
+				iCol >= 0 && iRow < BoardHeight && m_eTokens[idx] == eDropped;
+				--iCol, ++iRow, idx -= (BoardHeight - 1)
+			)
+			{
+				++iTokenCount;
+			}
+
+			// down right
+			for (
+				idx = iLastDropped + BoardHeight - 1,
+					iCol = iDropCol + 1,
+					iRow = iDropRow - 1;
+				iCol < BoardWidth && iRow >= 0 && m_eTokens[idx] == eDropped;
+				++iCol, --iRow, idx += BoardHeight - 1
+			)
+			{
+				++iTokenCount;
+			}
+
+
+			if (iTokenCount >= 4)
+			{
+				m_eWinner = eDropped;
+				return;
+			}
+		}
+
+	}
+
+}
---
+++ src/Board.hpp
@@ -0,0 +1,73 @@
+#ifndef ROBINLE_CONNECTFOUR_BOARD
+#define ROBINLE_CONNECTFOUR_BOARD
+
+
+
+
+
+#include <cassert>
+
+
+
+namespace rlConnectFour
+{
+
+	enum class Token : unsigned char
+	{
+		None,
+		Player,
+		AI
+	};
+
+
+
+	constexpr size_t BoardWidth  = 7;
+	constexpr size_t BoardHeight = 6;
+	constexpr size_t BoardSize   = BoardWidth * BoardHeight;
+
+
+	class Board
+	{
+	public: // methods
+
+		Token getToken(unsigned idx) const
+		{
+			assert(idx < BoardSize);
+			return m_eTokens[idx];
+		}
+		
+		unsigned getIndex(unsigned iColumn, unsigned iRowFromBottom) const;
+
+		Token getWinner() const { return m_eWinner; }
+
+
+		void dropToken(Token ePlayer, unsigned iColumn);
+
+
+	private: // methods
+
+		void checkWinner(size_t iLastDropped);
+
+
+	private: // variables
+
+		// the tokens array is indexed column by column, bottom to top:
+		// 
+		// |05|11|..|41|
+		// |..|..|..|..|
+		// |02|08|..|38|
+		// |01|07|..|37|
+		// |00|06|..|36|
+		Token  m_eTokens    [BoardSize ] = {};
+		size_t m_iTokenCount[BoardWidth] = {};
+		Token  m_eWinner                 = Token::None;
+
+	};
+
+}
+
+
+
+
+
+#endif // ROBINLE_CONNECTFOUR_BOARD
