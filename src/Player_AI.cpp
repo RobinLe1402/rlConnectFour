88519f92f37ff7fa174d470a01bc34533d840c7e
RobinLe1402 <web@robinle.de>
2024-06-18 21:49:45 +0200

    Player_AI: added console output

--- src/Player_AI.cpp
+++ src/Player_AI.cpp
@@ -1,16 +1,28 @@
 #include "Player_AI.hpp"
 
+#include <iostream>
+
 namespace rlConnectFour
 {
 
 	unsigned AIPlayer::nextMove()
 	{
+		std::cout << "Player " << m_iPlayerNo << " is thinking...\n";
+
+		unsigned iColumn;
+
 		// TODO: proper AI
 		for (unsigned i = 0; i < BoardWidth; ++i)
 		{
 			if (m_oBoard.hasRoom(i))
-				return i;
+			{
+				iColumn = i;
+				break; // for
+			}
 		}
+
+		std::cout << "Player " << m_iPlayerNo << " chose column " << (iColumn + 1) << ".\n";
+		return iColumn;
 	}
 
 }
