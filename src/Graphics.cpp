e930b0c0946eed34c0322988749fc9f257403576
RobinLe1402 <web@robinle.de>
2024-06-18 21:28:43 +0200

    added Graphics.cpp and Graphics.hpp

---
+++ src/Graphics.cpp
@@ -0,0 +1,41 @@
+#include "Graphics.hpp"
+#include "Console.hpp"
+
+namespace rlConnectFour
+{
+	std::ostream &operator<<(std::ostream &stream, const Board &board)
+	{
+		stream << " |1 |2 |3 |4 |5 |6 |7 |\n"
+		       << " +--+--+--+--+--+--+--+\n";
+
+		for (int iY = BoardHeight - 1; iY >= 0; --iY)
+		{
+			stream << " |";
+			for (int iX = 0; iX < BoardWidth; ++iX)
+			{
+				switch (board.getToken(board.getIndex(iX, BoardHeight - 1 - iY)))
+				{
+				case Token::None:
+					stream << "  ";
+					break;
+
+				case Token::Player1:
+					Console::SetForegroundColor(Console::Color::BrightRed);
+					stream << "()";
+					break;
+
+				case Token::Player2:
+					Console::SetForegroundColor(Console::Color::BrightYellow);
+					stream << "()";
+					break;
+				}
+				Console::ResetColors();
+				stream << "|\n";
+			}
+		}
+
+		stream << " +--+--+--+--+--+--+--+\n";
+		return stream;
+	}
+
+}
---
+++ src/Graphics.hpp
@@ -0,0 +1,24 @@
+#ifndef ROBINLE_CONNECTFOUR_GRAPHICS
+#define ROBINLE_CONNECTFOUR_GRAPHICS
+
+
+
+
+
+#include <iostream>
+#include "Board.hpp"
+
+
+
+namespace rlConnectFour
+{
+
+	std::ostream &operator<<(std::ostream &stream, const Board &board);
+
+}
+
+
+
+
+
+#endif // ROBINLE_CONNECTFOUR_GRAPHICS
