42e51ac07b675cc7569d51ac01b747b7a24dbac5
RobinLe1402 <web@robinle.de>
2024-06-18 18:10:08 +0200

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+	std::printf(
+		"==================================================\n"
+		"= ROBIN LE'S  CONNECT FOUR  FOR THE COMMAND LINE =\n"
+		"==================================================\n"
+		"\n"
+	);
+
+	return 0;
+}
